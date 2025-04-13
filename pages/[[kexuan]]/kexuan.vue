8b0b4dd2857c0678bea571f3254a424f9d84dfa7
yejiechen <1940879828@qq.com>
2025-04-13 10:46:35 +0800

    feat: 动态路由、命名路由、可选路由

---
+++ pages/[[kexuan]]/kexuan.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    可选路由:{{route.params.kexuan}}
+  </div>
+</template>
+
+<script setup lang="ts">
+// http://localhost:3000/123/kexuan
+const route = useRoute()
+console.log(route.params)
+</script>
+
+<style scoped>
+
+</style>
